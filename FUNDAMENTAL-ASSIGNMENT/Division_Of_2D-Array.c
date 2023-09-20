d047fc99ed82f286f429b526068357dc64d34823
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-09-20 20:31:08 +0530

    Add files via upload

---
+++ FUNDAMENTAL-ASSIGNMENT/Division_Of_2D-Array.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void main()
+{
+	int number1[2][2],number2[2][2],result[2][2],i,j;
+	
+	printf("Enter 4 values for First Array:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&number1[i][j]);
+		}
+	}
+	printf("Enter 4 values for Second Array:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			scanf("%d",&number2[i][j]);
+		}
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			result[i][j]=number1[i][j]/number2[i][j];
+		}
+	}
+	
+	printf("Your Result Array is:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",result[i][j]);
+		}
+		printf("\n");
+	}
+}
