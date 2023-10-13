9a7fe12ea9904c4fdfdd743d49243767fa9d35a0
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-10-13 16:44:06 +0530

    Update Concatenate_String_UsingOperatorOverloading.cpp

--- OOP AASIGNMENT/Concatenate_String_UsingOperatorOverloading.cpp
+++ OOP AASIGNMENT/Concatenate_String_UsingOperatorOverloading.cpp
@@ -2,49 +2,46 @@
 using namespace std;
 
 class concat{
-	
-	public:
-		char s1[50], s2[50];
+
+	string name1;
 		
-		void addstring(char str1[], str2[])
+		public:		
+		void addstring()
 		{
-			s1=str1[];
-			s2=str2[];
-			
-			cout<<"Enter String One:";
-			cin>>str1[];
-			cout<<"Enter String Two:";
-			cin>>str2[];
+			cin>>name1;
 		}
 		
 		void display()
 		{
-			cout<<"\n String Are"<<str1[]<<str2[]<<endl;
+			cout<<name1;
 		}
 		
 		void operator +(concat add)
 		{
-			concat s3;
-			s3.str1[]=str1[]+add.str1[];
-			s3.str2[]=str2[]+add.str2[];
-			cout<<"Concatenate String is: "<<s3.str1[]<<s3.str2[];
+			concat res;
+			res.name1=name1+" "+add.name1;
+			cout<<res.name1;
 		}
-	
+		
 };
 
 int main()
 {
+	
 	concat c1;
 	concat c2;
 	
+	cout<<"Enter The First String: ";
 	c1.addstring();
+	cout<<"Enter The Second String: ";
 	c2.addstring();
 	
-	cout<<"String One";
+	cout<<"\nFirst String: "<<endl;
 	c1.display();
-	cout<<"String Two";
+	cout<<"\n \nSecond String: "<<endl;
 	c2.display();
 	
+	cout<<endl<<"\nConcatenate String: "<<endl;
 	c1+c2;
 	
 	return 0;
